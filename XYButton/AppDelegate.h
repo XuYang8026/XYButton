eb51beeb500c719dd67e54f7b26aebc3cd26d42f
徐阳 <xuyang@yangs-macbook-pro.local>
2017-06-02 19:42:57 +0800

    Initial Commit

---
+++ XYButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYButton
+//
+//  Created by 徐阳 on 2017/6/2.
+//  Copyright © 2017年 徐阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYButton
+//
+//  Created by 徐阳 on 2017/6/2.
+//  Copyright © 2017年 徐阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
